2577be2218f1b9c058bc482e3590699b3a797984
Sara <sarasj0510@gmail.com>
2026-06-12 13:10:38 +0530

    Update

---
+++ insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (email, hashed_password, role, is_active, full_name) VALUES ('admin@sentry.com', '$2b$12$ICOZk4chqEFgX1h65rTyF.974EOtAMbOiz9FX0w68EvIm.OOhOSHC', 'admin', true, 'System Admin');
